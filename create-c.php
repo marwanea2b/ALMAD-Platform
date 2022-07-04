a235fbe2733d712fbeff6c4d9bdfa4416ca9eb89
marwane aarab <96587738+marwanea2b@users.noreply.github.com>
2022-07-04 12:28:12 +0100

    Add files via upload

---
+++ create-c.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+    include("includes/connection.php");
+    include("includes/functions.php");
+
+    $user_data= check_login($con);
+
+?>
+<?php
+
+$code = randomCode();
+$title =  $_REQUEST['title'];
+$description = $_REQUEST['description'];
+$teacher = $user_data['Fullname'];
+$teacher_id = $user_data['User_ID'];
+
+
+$sql = "INSERT INTO classes  VALUES ('$code', '$title', 
+    '$description', '$teacher', '$teacher_id')";
+
+$sql2 = "INSERT INTO relations  VALUES ('teacher', '$teacher_id', 
+    '$code')";
+
+if(mysqli_query($con, $sql) && mysqli_query($con, $sql2)){
+    echo "<h3>data stored in a database successfully. Here's your code : $code"; 
+    $_SESSION['status'] = 'Class created succefully';
+    $_SESSION['status_text'] = "Class code: $code";
+    $_SESSION['status_code'] = 'success';
+    header("Location: classes.php");
+} else{
+    echo "ERROR: Sorry $sql. " 
+        . mysqli_error($con);
+}
+
+mysqli_close($con);
+
+?>
